b0ad7e383bcf6616ad0b72ac352465bf74975452
itamarpambo <itamarpambo751@gmail.com>
2023-12-05 10:52:47 +0100

    Regex para password letras e numeros, pelo menos 8 caracteres

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
 
 //Nome
 var regex = /^[a-zA-Z ]+$/;
+
+//password letras e numero, 8 caracteres
+var regexSenha = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
